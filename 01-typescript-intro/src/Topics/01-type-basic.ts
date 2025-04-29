6078a12a3ea804b8b7e2f643b185399763555863
pcnay <pcnay2003@gmail.com>
2025-04-29 01:46:10 +0000

    Se definen los tipos de datos basicos

---
+++ 01-typescript-intro/src/Topics/01-type-basic.ts
@@ -0,0 +1,11 @@
+const name:string  = 'striper';
+let hpPoint:number | string = 95;
+hpPoint = 'FULL';
+const isAlive:boolean = true;
+
+console.log ({
+    name, hpPoint,isAlive
+})
+
+export{};
+
---
+++ 01-typescript-intro/src/main.ts
@@ -0,0 +1,6 @@
+import './style.css';
+import './Topics/01-type-basic.ts';
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+app.innerHTML = ` Hola Mundo `
+
